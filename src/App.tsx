191d6f5c18730e4f63ba053cd8b7f19ed2400c3c
Hamzah <dehamzah@gmail.com>
2025-03-09 17:24:15 +0700

    fix: use hash router because github pages didn't support server side routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { Route, Switch } from "wouter";
-
+import { Router, Route, Switch } from "wouter";
+import { useHashLocation } from "wouter/use-hash-location";
 import { ROUTE_TITLES } from "./constants";
 import HomePage from "./pages/HomePage";
 import DetailPage from "./pages/DetailPage";
@@ -11,14 +11,16 @@ const App = () => {
   return (
     <>
       <ThemeProvider defaultTheme="system" storageKey="theme">
-        <Switch>
-          <Route path="/" component={HomePage} />
-          {Object.keys(ROUTE_TITLES).map((routePath) => (
-            <Route key={routePath} path={routePath} component={DetailPage} />
-          ))}
-          <Route path="/settings" component={SettingsPage} />
-          <Route component={NotFoundPage} />
-        </Switch>
+        <Router hook={useHashLocation}>
+          <Switch>
+            <Route path="/" component={HomePage} />
+            {Object.keys(ROUTE_TITLES).map((routePath) => (
+              <Route key={routePath} path={routePath} component={DetailPage} />
+            ))}
+            <Route path="/settings" component={SettingsPage} />
+            <Route component={NotFoundPage} />
+          </Switch>
+        </Router>
       </ThemeProvider>
     </>
   );
